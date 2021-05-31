57750d75d964c89b851aefb776368734030ff7ba
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-31 11:07:05 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// Ternary Operators
+let points = 110;
+
+let customerType = points > 100 ? "gold" : "silver";
+// let variable = condition ? ifTrue : ifFalse;
+
+// Operator Precedence
+let x = 2 + 3 * 4; // returns as 14
+
+let x = (2 + 3) * 4; // returns as 20
+
+// operator precendence is similar in maths.
